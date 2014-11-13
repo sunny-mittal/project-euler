b379280fe24b9538b5f762bbfb9ba597069beea2
Sunny <Sunny Mittal>
2014-11-12 21:40:06 -0800

    Finish question 3

---
+++ ruby/three.rb
@@ -0,0 +1,16 @@
+def prime_factorization(n)
+  results = []
+  if n.even?
+    results << 2
+    n /= 2 until n.odd?
+  end
+
+  (3..n).step(2) do |i|
+    if n % i == 0
+      results << i
+      n /= i until n % i != 0
+    end
+    break if i > n
+  end
+  results.join(', ')
+end

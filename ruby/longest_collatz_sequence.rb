bb8de7f6a2a62ec2cb620b916c2dc33bae83aa53
Sunny Mittal <sunnymittal2003@gmail.com>
2014-11-14 23:32:11 -0800

    WIP

---
+++ ruby/longest_collatz_sequence.rb
@@ -0,0 +1,25 @@
+def generate_sequence(n)
+  count = 0
+  loop do  
+    count += 1
+    case
+    when n == 1 then break
+    when n.odd? then n = 3 * n + 1
+    when n.even? then n /= 2
+    end
+  end
+  count
+end
+
+def find_longest(limit)
+  max = seed = 0
+  (1...limit).step(2) do |test|
+    if (x = generate_sequence(test)) > max
+      max, seed = x, test
+    end
+  end
+  [max, seed]
+end
+
+puts find_longest 1_000_000
+

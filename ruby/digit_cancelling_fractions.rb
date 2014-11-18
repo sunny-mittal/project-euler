f06f66658c3cedd3919101182cbe9201c7a01593
Sunny Mittal <sunnymittal2003@gmail.com>
2014-11-18 01:57:13 -0800

    Refactor a little

--- ruby/digit_cancelling_fractions.rb
+++ ruby/digit_cancelling_fractions.rb
@@ -4,21 +4,20 @@ arr = []
 
 (10..98).each do |num|
   (num + 1..99).each do |den|
-    num_array = num.to_s.split('')
-    den_array = den.to_s.split('')
+    num_array, den_array = num.to_s.split(''), den.to_s.split('')
     next if (num_array - den_array).length == 2 || num % 10 == 0 && den % 10 == 0
     val = num / den.to_f
     common = (num_array & den_array).first
-    num_array.delete(common)
-    den_array.delete(common)
-    num_array << common if num_array.empty?
-    den_array << common if den_array.empty?
-    numer = num_array.first.to_i
-    denom = den_array.first.to_i
-    n = numer / numer.gcd(denom)
-    d = denom / numer.gcd(denom)
-    next if denom == 0
-    arr << [n, d] if numer / denom.to_f == val
+    [num_array, den_array].each do |arr|
+      arr.delete(common)
+      arr << common if arr.empty?
+    end
+    n, d = [num_array.first, den_array.first].map(&:to_i)
+    gcd = n.gcd(d)
+    n /= gcd
+    d /= gcd
+    next if d == 0
+    arr << [n, d] if n / d.to_f == val
   end
 end
 

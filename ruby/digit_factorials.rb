4fdcc0e4058ca152881f4da02d0250fd9a059363
Sunny Mittal <sunnymittal2003@gmail.com>
2014-11-22 23:16:36 -0800

    WIP

--- ruby/digit_factorials.rb
+++ ruby/digit_factorials.rb
@@ -2,10 +2,18 @@ require_relative './lib/extended_fixnum'
 
 class Fixnum
   def factorial_sum
-    to_s.split('').map(&:to_i).reduce(0) { |acc, e| acc + e.factorial }
+    to_s.split('').map(&:to_i).reduce(0) { |acc, e| acc + ::Factorials[e] }
   end
+end
+
+Factorials = {}
+
+(0..9).each do |n|
+  Factorials[n] = n.factorial
 end
 
 def find_sum
   (3..2540160).select { |x| x == x.factorial_sum } .reduce(:+)
 end
+
+puts find_sum

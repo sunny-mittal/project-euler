113cf2216bc4a317e33a273da5ce0ce7ecd2f4a0
Sunny Mittal <sunnymittal2003@gmail.com>
2014-11-23 00:24:10 -0800

    WIP

---
+++ ruby/double-base_palindromes.rb
@@ -0,0 +1,13 @@
+def palindromes(limit)
+  (1...limit).select do |x|
+    str = x.to_s
+    str == str.reverse
+  end
+end
+
+x = palindromes(1000000).select do |x|
+  str = x.to_s(2)
+  str == str.reverse
+end.reduce(:+)
+
+puts x
